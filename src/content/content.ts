6bc263033f2bf7a79029e665271d048d4af27997
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-11-30 21:47:15 +0100

    Nu i govno

--- src/content/content.ts
+++ src/content/content.ts
@@ -24,23 +24,98 @@ function highlightSelection(selection: Selection, range: Range, color: string) {
     }
 }
 
-function fetchJoke() {
+function sendSentenceRequest(sentence: string) {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            replaceSelectionWithJoke(xhr.responseText);
+            console.log(xhr.responseText);
         }
     };
-    xhr.open("GET", "https://icanhazdadjoke.com/", true);
+
+    const encodedText = encodeURIComponent(sentence);
+    const url = `http://34.85.198.249:12023/text_predict?text=${encodedText}`;
+    xhr.open("GET", url, true);
     xhr.setRequestHeader("Accept", "text/plain");
     xhr.send();
+}
+
+function selectionToSentences(selection: Selection): string[] {
+    const range = selection.getRangeAt(0);
+    const text = range.toString();
+    return text
+        .split('.')
+        .map(s => s.trim())
+        .filter(s => s.length > 0)
+        .map(s => s + '.');
+}
+
+function getTextNodes(range: Range) {  
+    const nodeList: Node[] = [];
+    // Iterate over all the child nodes in the document fragment
+    const iterateChildNodes = (node: Node) => {
+        node.childNodes.forEach((child) => {
+            if (child.nodeType === Node.TEXT_NODE) {
+                nodeList.push(child);
+            }
+            if (child.hasChildNodes()) {
+                iterateChildNodes(child);
+            }
+        });
+    };
+
+    iterateChildNodes(range.extractContents());
+
+    return nodeList;
+}
+
+function getSubstringOffsetInString(string: string, substring: string): number {
+    const index = string.indexOf(substring);
+    if (index >= 0) {
+        return index;
+    }
+    return 0;
+}
+
+function randomBool() {
+    return Math.random() < 0.5;
+}
+
+function checkSentence(sentence: string): boolean {
+    sendSentenceRequest(sentence);
+    return randomBool();
+}
+
+function highlightSentence(selection: Selection, sentence: string, text: string) {
+    const range = selection.getRangeAt(0);
+    const startContainer = range.startContainer;
+    const endContainer = range.endContainer;
+    if (startContainer !== endContainer) {
+        console.log('Invalid selection');
+        return;
+    }
+    const startOffset = range.startOffset;
+    const sentenceOffset = getSubstringOffsetInString(text, sentence);
+    
+    const newStartOffset = startOffset + sentenceOffset;
+    const newEndOffset = newStartOffset + sentence.length;
+
+    range.setStart(startContainer, newStartOffset);
+    range.setEnd(startContainer, newEndOffset);
+    
+    highlightSelection(selection, range, 'yellow');
 }
 
 function mainNode() {
     const selection = window.getSelection();
     const text = selection?.toString();
     if (selection && text && text.length > 0) {
-        fetchJoke();
+        const sentences = selectionToSentences(selection);
+
+        for (const sentence of sentences) {
+            if (checkSentence(sentence)) {
+                highlightSentence(selection, sentence, text);
+            }
+        }
     }
 }
 
